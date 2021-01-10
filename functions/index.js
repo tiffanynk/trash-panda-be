8f867b440d7bb45631ce1c851c7f25afb590e7d5
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2021-01-09 17:29:02 -0700

    registration added and cors

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,18 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-
+const app = require('express')();
+const firebaseConfig = require('../firebaseConfig');
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const firebase = require('firebase');
+const database = admin.firestore();
+firebase.initializeApp(firebaseConfig);
+const cors = require('cors');
+const { ResultStorage } = require('firebase-functions/lib/providers/testLab');
+app.use(cors());
 
 app.get('/locations', (request, response) => {
-    admin.firestore().collection('Locations').get()
+    database.collection('Locations').get()
         .then(data => {
             let locations = []
             data.forEach(document => {
@@ -30,7 +35,7 @@ app.post('/location', (request, response) => {
         recycling: request.body.recycling
     }
 
-    admin.firestore().collection('Locations').add(newLocation)
+    database.collection('Locations').add(newLocation)
         .then(document => {
             let createdLocation = newLocation;
             createdLocation.locationId = document.id;
@@ -40,6 +45,39 @@ app.post('/location', (request, response) => {
             response.status(500).json({ error: 'Something went wrong!' });
             console.error(error);
     })
+})
+
+app.post('/signup', (request, response) => {
+    const newUser = {
+        email: request.body.email,
+        password: request.body.password,
+        username: request.body.username
+    };
+
+    database.doc(`users/${newUser.username}`).get()
+        .then(document => {
+            if(document.exists){
+                return response.status(400).json({ username: 'This username is already taken.'})
+            } else {
+                return firebase
+                        .auth()
+                        .createUserWithEmailAndPassword(newUser.email, newUser.password)
+            }
+        })
+        .then(data => {
+            return data.user.getIdToken()
+        })
+        .then(token => {
+            return response.status(201).json({ token })
+        })
+        .catch(error => {
+            console.error(error);
+            if(error.code !== null){
+                return response.status(400).json({ user: 'This username or email is already in use.' });
+            } else {
+                return response.status(500).json({ error: error.code });
+            }
+        });    
 })
 
 //turns all of our functions into multiple routes
