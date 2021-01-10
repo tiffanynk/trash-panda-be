0d0d3a3b9bf1eb608804c6475c7c35670b81322d
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2021-01-10 05:47:39 -0700

    fixed signup for fe

---
+++ functions/firebaseConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyAyp-4lszmeiiu5QIGWTyOBiZGH3J2kH-M",
+    authDomain: "trash-panda-shehacks.firebaseapp.com",
+    projectId: "trash-panda-shehacks",
+    storageBucket: "trash-panda-shehacks.appspot.com",
+    messagingSenderId: "746729791095",
+    appId: "1:746729791095:web:268be4d973012825c0f072",
+    measurementId: "G-DMDH5Y8T6C"
+};
--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,12 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const app = require('express')();
-const firebaseConfig = require('../firebaseConfig');
+const firebaseConfig = require('./firebaseConfig');
 admin.initializeApp();
-
 const firebase = require('firebase');
 const database = admin.firestore();
 firebase.initializeApp(firebaseConfig);
 const cors = require('cors');
-const { ResultStorage } = require('firebase-functions/lib/providers/testLab');
 app.use(cors());
 
 app.get('/locations', (request, response) => {
@@ -35,7 +33,8 @@ app.post('/location', (request, response) => {
         recycling: request.body.recycling
     }
 
-    database.collection('Locations').add(newLocation)
+    database.collection('Locations')
+        .add(newLocation)
         .then(document => {
             let createdLocation = newLocation;
             createdLocation.locationId = document.id;
@@ -108,14 +107,16 @@ app.post('/signup', (request, response) => {
             const userCredentials = {
                 username: newUser.username,
                 email: newUser.email,
+                totalPoints: 0,
                 userId
             }
             database.doc(`/users/${newUser.username}`)
                 .set(userCredentials)
+            return response.status(201).json({user: {email: userCredentials.email}, token})
         })
-        .then(() => {
-            return response.status(201).json({ token })
-        })
+        // .then(() => {
+        //     return response.status(201).json({newUser: {email: user.email}, token })
+        // })
         .catch(error => {
             console.error(error);
             if(error.code === 'auth/email-already-in-use'){
@@ -153,7 +154,7 @@ app.post('/login', (request, response) => {
             return data.user.getIdToken()
         })
         .then(token => {
-            response.json({ token})
+            response.json({user: {email: user.email}, token })
         })
         .catch(error => {
             console.error(error)
@@ -163,6 +164,7 @@ app.post('/login', (request, response) => {
                 return response.status(500).json({ error: error.code })
             }
         })
+
 })
 //turns all of our functions into multiple routes
 exports.api = functions.https.onRequest(app);
